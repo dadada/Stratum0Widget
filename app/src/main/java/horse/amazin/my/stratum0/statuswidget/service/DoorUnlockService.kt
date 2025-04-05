ac6b12c2260b899baae7acc816fd69b3255ecd88
Tim Schubert <dadada@dadada.li>
2025-04-05 23:09:41 +0200

    set IP addrs for space 2 and space 3 door openers

--- app/src/main/java/horse/amazin/my/stratum0/statuswidget/service/DoorUnlockService.kt
+++ app/src/main/java/horse/amazin/my/stratum0/statuswidget/service/DoorUnlockService.kt
@@ -75,8 +75,8 @@ class DoorUnlockService : IntentService("Space Door Service") {
     private fun findHostForLocation(location: String?): String =
         if (BuildConfig.DEBUG) "192.168.178.21" else {
             when(location) {
-                UPPER_LOCATION -> "" // TODO host to be determined yet
-                LOWER_LOCATION -> "basilisk"
+                UPPER_LOCATION -> "192.168.178.5" // TODO host to be determined yet
+                LOWER_LOCATION -> "192.168.178.6"
                 else -> "basilisk"
             }
         }
